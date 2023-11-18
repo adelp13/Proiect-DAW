f51c531ddcc0a30ce7d7142c406fd3b3061ab0e2
Alexandru Miclea <alexandrumiclea@outlook.com>
2023-11-18 15:14:24 +0200

    Added the Profile, Message and Group models

---
+++ Cod/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cod.Models
+{
+    public class Profile : IdentityUser
+    {
+        public bool isPrivate { get; set; }
+        public virtual ICollection<Group> Groups { get; set; }
+        // TODO: create self M-M tables
+        //public virtual ICollection<Profile> Follows { get; set; }
+        //public virtual ICollection<Profile> Follow_Requests { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
